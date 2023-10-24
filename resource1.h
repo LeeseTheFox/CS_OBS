159f06ba9f862a9cb7524a1b62468ba9d373c7e6
LeeseTheFox <azavladyslav@gmail.com>
2023-10-24 23:50:54 +0300

    Icon update

--- resource1.h
+++ resource1.h
@@ -3,14 +3,15 @@
 // Used by CS_OBS1.rc
 //
 #define IDI_ICON2                       102
-#define IDI_ICON1                       102
 #define IDI_ICON3                       103
+#define IDI_ICON1                       103
+#define IDI_ICON4                       105
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
